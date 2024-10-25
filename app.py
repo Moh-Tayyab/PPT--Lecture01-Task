9dbad960d411751c46001abe8b002a6374a7f23c
Muhammad-Tayyab <m.tayyab1263@gmail.com>
2024-10-25 23:22:00 +0500

    python update....

---
+++ app.py
@@ -0,0 +1,19 @@
+# Small Task 
+# Task: Take two numbers as input and print their sum.
+# Input: Two numbers
+# Integer
+num1 = int(input('Enter First Number: '));
+
+num2 = int(input('Enter Second Number: '));
+
+sum = num1 + num2;
+
+print('Sum of two numbers is: ', sum);
+#Float
+num01 = float(input('Enter First Float Number: '));
+
+num02 = float(input('Enter Second Float Number: '));
+
+sum1 = num01 + num02;
+
+print('Sum of two numbers is: ', sum1);
